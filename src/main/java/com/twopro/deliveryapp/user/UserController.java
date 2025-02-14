f3387a986fc02f7cfd3a30f04c131b5eaff2417b
dmstjs915 <dmstjs915@gmail.com>
2025-02-14 10:07:59 +0900

    feat: user 초기 엔티티 및 컨트롤러 생성 #4

--- src/main/java/com/twopro/deliveryapp/user/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.twopro.deliveryapp.user;
-
-public class UserController {
-}
---
+++ src/main/java/com/twopro/deliveryapp/user/entity/User.java
@@ -0,0 +1,44 @@
+package com.twopro.deliveryapp.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "p_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    private String email;
+    private String password;
+    private String nickname;
+    private String role;
+    private String province;
+    private String district;
+    private String town;
+    private String road_address;
+    private String detail_address;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created_at;
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated_at;
+    private Date deleted_at;
+    private String created_by;
+    private String updated_by;
+    private String deleted_by;
+}
