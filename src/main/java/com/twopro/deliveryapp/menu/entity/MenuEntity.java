82757c333f07eb106bceb0b2704c3e89deb80bde
zun <k5orkrj@gmail.com>
2025-02-17 23:37:24 +0900

    feat: add static method to update menu in MenuEntity

--- src/main/java/com/twopro/deliveryapp/menu/entity/MenuEntity.java
+++ src/main/java/com/twopro/deliveryapp/menu/entity/MenuEntity.java
@@ -1,14 +1,18 @@
 package com.twopro.deliveryapp.menu.entity;
 
 import com.twopro.deliveryapp.common.entity.BaseEntity;
+import com.twopro.deliveryapp.menu.dto.AddMenuRequestDto;
+import com.twopro.deliveryapp.menu.dto.UpdateMenuRequestDto;
 import com.twopro.deliveryapp.store.entity.Store;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.UuidGenerator;
 
 @Entity
 @Table(name = "P_MENU")
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MenuEntity extends BaseEntity {
@@ -31,9 +35,18 @@ public class MenuEntity extends BaseEntity {
     // TODO s3? test url?
     private String imageUrl;
 
-    @Column(length = 255)
     private String description;
 
     @Column(nullable = false)
     private int price;
+
+    public static MenuEntity update(MenuEntity menuEntity, UpdateMenuRequestDto updateMenuRequestDto) {
+        menuEntity.name = updateMenuRequestDto.name();
+        menuEntity.status = updateMenuRequestDto.status();
+        menuEntity.imageUrl = updateMenuRequestDto.imageUrl();
+        menuEntity.description = updateMenuRequestDto.description();
+        menuEntity.price = updateMenuRequestDto.price();
+
+        return menuEntity;
+    }
 }
