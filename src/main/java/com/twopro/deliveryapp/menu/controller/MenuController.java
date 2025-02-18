30e28e9e3385b43ecbcd12ca2531cc0d8c33eb77
zun <k5orkrj@gmail.com>
2025-02-18 12:22:50 +0900

    refactor: change service return type for controller responsibility #3

--- src/main/java/com/twopro/deliveryapp/menu/controller/MenuController.java
+++ src/main/java/com/twopro/deliveryapp/menu/controller/MenuController.java
@@ -1,7 +1,25 @@
 package com.twopro.deliveryapp.menu.controller;
 
+import com.twopro.deliveryapp.common.dto.SingleResponse;
+import com.twopro.deliveryapp.menu.dto.AddMenuRequestDto;
+import com.twopro.deliveryapp.menu.dto.MenuResponseDto;
+import com.twopro.deliveryapp.menu.service.MenuService;
+import com.twopro.deliveryapp.store.entity.Store;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/menus")
 public class MenuController {
+
+    private final MenuService menuService;
+
+    @PostMapping
+    public SingleResponse<MenuResponseDto> addMenu(AddMenuRequestDto addMenuRequestDto, Store store) {
+
+        return null;
+    }
 }
