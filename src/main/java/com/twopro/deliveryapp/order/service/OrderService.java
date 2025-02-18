2b06d0cda5d30512741e590d31f4453659587d4e
jnjongjeon <jeonij23@gmail.com>
2025-02-18 12:19:28 +0900

    fix: MenuEntity 이름 변경

--- src/main/java/com/twopro/deliveryapp/order/service/OrderService.java
+++ src/main/java/com/twopro/deliveryapp/order/service/OrderService.java
@@ -1,28 +1,14 @@
 package com.twopro.deliveryapp.order.service;
 
-import com.twopro.deliveryapp.common.entity.Address;
-import com.twopro.deliveryapp.menu.entity.MenuEntity;
-import com.twopro.deliveryapp.menu.repository.MenuRepository;
-import com.twopro.deliveryapp.order.dto.CreateOrderMenuDto;
-import com.twopro.deliveryapp.order.dto.FindOrderResponseDto;
 import com.twopro.deliveryapp.order.dto.OrderCreateRequestDto;
-import com.twopro.deliveryapp.order.dto.OrderMenuResponseDto;
 import com.twopro.deliveryapp.order.entity.Order;
 import com.twopro.deliveryapp.order.excepiton.OrderAccessDeniedException;
 import com.twopro.deliveryapp.order.repository.OrderRepository;
-import com.twopro.deliveryapp.orderItem.Entity.OrderItem;
-import com.twopro.deliveryapp.user.entity.User;
 import com.twopro.deliveryapp.user.repository.UserRepository;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
