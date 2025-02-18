fcb5091c7a81f8f4887cdbd07d92fc89761ba688
zun <k5orkrj@gmail.com>
2025-02-18 13:07:39 +0900

    feat: add menu crud controller methods #3

--- src/main/java/com/twopro/deliveryapp/menu/controller/MenuController.java
+++ src/main/java/com/twopro/deliveryapp/menu/controller/MenuController.java
@@ -3,12 +3,13 @@ package com.twopro.deliveryapp.menu.controller;
 import com.twopro.deliveryapp.common.dto.SingleResponse;
 import com.twopro.deliveryapp.menu.dto.AddMenuRequestDto;
 import com.twopro.deliveryapp.menu.dto.MenuResponseDto;
+import com.twopro.deliveryapp.menu.dto.UpdateMenuRequestDto;
 import com.twopro.deliveryapp.menu.service.MenuService;
-import com.twopro.deliveryapp.store.entity.Store;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +19,39 @@ public class MenuController {
     private final MenuService menuService;
 
     @PostMapping
-    public SingleResponse<MenuResponseDto> addMenu(AddMenuRequestDto addMenuRequestDto, Store store) {
+    public SingleResponse<MenuResponseDto> addMenu(@RequestBody AddMenuRequestDto addMenuRequestDto) {
+        MenuResponseDto addMenuResponse = menuService.addMenu(addMenuRequestDto);
+        return SingleResponse.success(addMenuResponse);
+    }
 
-        return null;
+    // TODO 관리자를 위한 단순 메뉴 전체 조회 필요할지 고민
+
+    @GetMapping("/store")
+    public SingleResponse<List<MenuResponseDto>> findAllMenuByStoreId(@RequestParam UUID storeId) {
+        List<MenuResponseDto> findMenuResponseDtoList = menuService.findAllMenuByStoreId(storeId);
+
+        return SingleResponse.success(findMenuResponseDtoList);
+    }
+
+    @GetMapping("/name")
+    public SingleResponse<List<MenuResponseDto>> findAllMenuByName(@RequestParam String name) {
+        List<MenuResponseDto> findMenuResponseDtoList = menuService.findAllMenuByName(name);
+
+        return SingleResponse.success(findMenuResponseDtoList);
+    }
+
+    @PatchMapping("/{menuId}")
+    public SingleResponse<MenuResponseDto> updateMenu(
+            @PathVariable UUID menuId,
+            @RequestBody UpdateMenuRequestDto updateMenuRequestDto
+    ) {
+        MenuResponseDto updateMenuResponse = menuService.updateMenu(menuId, updateMenuRequestDto);
+
+        return SingleResponse.success(updateMenuResponse);
+    }
+
+    @PatchMapping("/delete/{menuId}")
+    public void deleteMenu(@PathVariable UUID menuId) {
+        menuService.deleteMenu(menuId);
     }
 }
