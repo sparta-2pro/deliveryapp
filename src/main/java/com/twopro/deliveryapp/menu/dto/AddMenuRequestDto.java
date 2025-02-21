47d91e5ade7a7b853e5feadb3a3046c03c254874
zun <k5orkrj@gmail.com>
2025-02-21 15:47:14 +0900

    refactor: use storeId instead of store entity in AddMenuRequestDto for efficiency

--- src/main/java/com/twopro/deliveryapp/menu/dto/AddMenuRequestDto.java
+++ src/main/java/com/twopro/deliveryapp/menu/dto/AddMenuRequestDto.java
@@ -3,10 +3,11 @@ package com.twopro.deliveryapp.menu.dto;
 import com.twopro.deliveryapp.menu.entity.MenuStatus;
 import com.twopro.deliveryapp.store.entity.Store;
 
+import java.util.UUID;
+
 // 가게 사장이 메뉴 등록 -> 유저 정보, 가게 정보 모두 있음
 public record AddMenuRequestDto (
-        // TODO StoreRequestDto 받아야 함
-        Store store,
+        UUID storeId,
         String name,
         MenuStatus status,
         String imageUrl,
