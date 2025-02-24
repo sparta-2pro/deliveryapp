37ffac28b3f2007d2ab952750936dda5d5a167c3
zun <k5orkrj@gmail.com>
2025-02-24 19:58:31 +0900

    refactor: use nativeQuery for study

--- src/main/java/com/twopro/deliveryapp/ai/repository/JpaAiRepository.java
+++ src/main/java/com/twopro/deliveryapp/ai/repository/JpaAiRepository.java
@@ -10,32 +10,31 @@ import java.util.UUID;
 
 public interface JpaAiRepository extends JpaRepository<Ai, UUID> {
 
+    // TODO 쿼리 최적화 필요함
     @Query(
             value =
-                    "SELECT DISTINCT * " +
-                            "FROM Ai AS a " +
-                            "JOIN FETCH a.menu AS m " +
-                            "JOIN FETCH m.store AS s " +
-                            "WHERE s.store_id = :storeId " +
-                            "ORDER BY a.created_at DESC " +
-                            "LIMIT :limit",
-
+                "SELECT DISTINCT a.* " +
+                "FROM P_Ai a " +
+                "LEFT JOIN P_MENU AS m ON a.menu_id = m.menu_id " +
+                "LEFT JOIN P_STORE AS s ON m.store_id = s.store_id" +
+                "WHERE s.store_id = :storeId " +
+                "ORDER BY a.created_at DESC " +
+                "LIMIT :limit",
             nativeQuery = true
     )
     List<Ai> findAllAiServiceByStoreId(@Param("storeId") UUID storeId, @Param("limit") Long size);
 
-
+    // TODO 쿼리 최적화 필요함
     @Query(
             value =
-                    "SELECT DISTINCT a " +
-                    "FROM Ai a " +
-                    "JOIN FETCH a.menu m " +
-                    "JOIN FETCH m.store s " +
+                    "SELECT DISTINCT a.* " +
+                    "FROM P_Ai a " +
+                    "LEFT JOIN P_MENU AS m ON a.menu_id = m.menu_id " +
+                    "LEFT JOIN P_STORE AS s ON m.store_id = s.store_id" +
                     "WHERE s.store_id = :storeId " +
                     "AND a.ai_id < :lastAiId " +
                     "ORDER BY a.created_at DESC " +
                     "LIMIT :limit",
-
             nativeQuery = true
     )
     List<Ai> findAllAiServiceByStoreId(
